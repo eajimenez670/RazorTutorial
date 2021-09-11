d07568c26ceba5704c7b899360e29f6b8768cff6
eajimenez670 <edisonj@digitalware.com.co>
2021-09-10 19:36:22 -0500

    Initial Commit.

---
+++ Models/SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                // Look for any movies
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Annie Hall",
+                        ReleaseDate = DateTime.Parse("1982-2-28"),
+                        Genre = "Romantic",
+                        Price = 20.000M
+                    },
+                    new Movie
+                    {
+                        Title = "Espartaco",
+                        ReleaseDate = DateTime.Parse("1960-6-17"),
+                        Genre = "Action",
+                        Price = 60.000M
+                    },
+                    new Movie
+                    {
+                        Title = "Irreversible",
+                        ReleaseDate = DateTime.Parse("2002-9-18"),
+                        Genre = "No Fiction",
+                        Price = 20.000M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
