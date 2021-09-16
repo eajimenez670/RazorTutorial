0f52cb58d601192c44301d48888419c0016816e4
eajimenez670 <edisonj@digitalware.com.co>
2021-09-16 13:09:05 -0500

    Complete the seven part of tutorial Razor Pages for Microsoft.

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -26,21 +26,24 @@ namespace RazorPagesMovie.Models
                         Title = "Annie Hall",
                         ReleaseDate = DateTime.Parse("1982-2-28"),
                         Genre = "Romantic",
-                        Price = 20.000M
+                        Price = 20.000M,
+                        Rating = "R"
                     },
                     new Movie
                     {
                         Title = "Espartaco",
                         ReleaseDate = DateTime.Parse("1960-6-17"),
                         Genre = "Action",
-                        Price = 60.000M
+                        Price = 60.000M,
+                        Rating = "E"
                     },
                     new Movie
                     {
                         Title = "Irreversible",
                         ReleaseDate = DateTime.Parse("2002-9-18"),
                         Genre = "No Fiction",
-                        Price = 20.000M
+                        Price = 20.000M,
+                        Rating = "E"
                     }
                 );
                 context.SaveChanges();
